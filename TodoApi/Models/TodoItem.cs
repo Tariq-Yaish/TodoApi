1147cb1815697ae11b541c6f38374654b8b9e509
Admin <Admin@TARIQYAISH>
2022-08-23 13:04:58 +0300

    Add project files.

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        //This functions as the primary key in a database.
+
+        public string? Name { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
---
+++ TodoApi/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+//builder.Services.AddSwaggerGen(c =>
+//{
+//    c.SwaggerDoc("v1", new() { Title = "TodoApi", Version = "v1" });
+//});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    //app.UseSwagger();
+    //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
